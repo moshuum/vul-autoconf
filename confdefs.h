d1f266c9cd7a5e43eff90ddff292fbb281bf380d
moshuum <jinan62@gmail.com>
2018-06-12 03:31:50 -0700

    compiling
    	new file:   Makefile.am
    	modified:   configure.ac
    	new file:   src/Makefile
    	new file:   src/Makefile.am
    	modified:   .gitignore

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "HelloWorld"
+#define PACKAGE_TARNAME "helloworld"
+#define PACKAGE_VERSION "0.01a"
+#define PACKAGE_STRING "HelloWorld 0.01a"
+#define PACKAGE_BUGREPORT "REPORT-ADDRESS"
+#define PACKAGE_URL ""
+#define PACKAGE "helloworld"
+#define VERSION "0.01a"
---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "helloworld"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "REPORT-ADDRESS"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "HelloWorld"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "HelloWorld 0.01a"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "helloworld"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.01a"
+
+/* Version number of package */
+#define VERSION "0.01a"

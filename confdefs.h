97b7beefac76bbbf3a4fcfe8ca9d0cd4e5d8025b
moshuum <jinan62@gmail.com>
2018-06-14 00:32:37 -0700

    re-init

--- confdefs.h
+++
@@ -1,9 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "HelloWorld"
-#define PACKAGE_TARNAME "helloworld"
-#define PACKAGE_VERSION "0.01a"
-#define PACKAGE_STRING "HelloWorld 0.01a"
-#define PACKAGE_BUGREPORT "REPORT-ADDRESS"
-#define PACKAGE_URL ""
-#define PACKAGE "helloworld"
-#define VERSION "0.01a"

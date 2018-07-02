5e179422d9ab154012a3e9e6183d20ffa4eb14ac
moshuum <jinan62@gmail.com>
2018-07-02 09:51:31 -0700

    update linker

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ extern "C" {
 int main(int argc, char *argv[]){
 
    std::cout << "Hello World!" << std::endl;
-
+   std::cout << "SQLite3 version " << sqlite3_libversion() << std::endl;
    return 0;
 
 }
